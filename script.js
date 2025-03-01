c63b00e86bb8bf29b48f46c0650d10ac4d7927ad
Ritesh Zambare <riteshzambare9@apsit.edu.in>
2025-03-01 22:37:33 +0530

    Updated files

--- script.js
+++ script.js
@@ -10,17 +10,12 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
-]
+    {songName: "Ishq Hai", filePath: "songs/1.mp3", coverPath: "covers/ishq_hai_img.jpeg"},
+    {songName: "Jo  Tum Mere Ho", filePath: "songs/2.mp3", coverPath: "covers/jo_tum_mere_ho_img.jpeg"},
+    {songName: "Ishq", filePath: "songs/3.mp3", coverPath: "covers/ishq_img.jpeg"},
+    {songName: "Sanam Teri Kasam", filePath: "songs/4.mp3", coverPath: "covers/sanam_teri_kasam_img.jpeg"},
+    {songName: "Sajni", filePath: "songs/5.mp3", coverPath: "covers/sajni_img.jpeg"},
+    ]
 
 songItems.forEach((element, i)=>{ 
     element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
@@ -100,7 +95,7 @@ document.getElementById('previous').addEventListener('click', ()=>{
     else{
         songIndex -= 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
+    audioElement.src = songs[songIndex].filePath;2
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
